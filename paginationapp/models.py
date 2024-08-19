c02f696de71537af9f9c6932237adc145428cee4
Awais Raheem <awais.raheem91@gmail.com>
2024-08-19 16:51:19 +0500

    added Flower Model

--- paginationapp/models.py
+++ paginationapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Flower(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="media")
+    
+    def __str__(self):
+        return self.name

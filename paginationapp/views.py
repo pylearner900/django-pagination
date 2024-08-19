7ea3e5a196d7fd7be97b86226a5ebdcbf4d7767e
Awais Raheem <awais.raheem91@gmail.com>
2024-08-19 17:13:00 +0500

    pagination setup in views.py

--- paginationapp/views.py
+++ paginationapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from .models import Flower
 
 # Create your views here.
 def index(request):
-    return render(request,"paginationapp/index.html")
+    flowers = Flower.objects.all()
+    f = Paginator(flowers,5)
+    page_number = request.GET['page']
+    try:
+        page_obj = f.page(page_number)
+    except PageNotAnInteger:
+        page_obj = f.page(1)
+    except EmptyPage:
+        page_obj = f.page(f.num_pages)
+    context = {"page_obj",page_obj}
+    return render(request,"paginationapp/index.html",context)
